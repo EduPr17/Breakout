0f461e396536cde2adc641236c6755e03c2fd856
EduPr17 <31894505+EduPr17@users.noreply.github.com>
2018-05-25 11:26:37 -0400

    Update main.py

--- main.py
+++ main.py
@@ -10,6 +10,16 @@ class BreakoutApp(App):
   def build(self):
     return BreakoutGame()
 
+class BreakoutBall(Widget):
+  
+    velocity_x = NumericProperty(0)
+    velocity_y = NumericProperty(0)
+    
+    
+    velocity = ReferenceListProperty(velocity_x, velocity_y)
+    
+    def move(self):
+      self.pos = Vector(*self.velocity) + self.pos
 
 if __name__ == '__main__':
   BreakoutApp().run()
