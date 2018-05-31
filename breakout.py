daee93d8e600a5c177bad8c2ad7d922247835c60
EduPr17 <31894505+EduPr17@users.noreply.github.com>
2018-05-31 19:18:11 -0400

    Update breakout.py

--- breakout.py
+++ breakout.py
@@ -1,54 +1,72 @@
 import pygame
 #SCREEN SIZE
-screen_size = 650, 490
-# DEFINING THE STRUCTURES 
-brick_width = 65
-brick_height = 49
-paddle_width = 65
-paddle_height = 49
-ball_diameter = 16
+screen_size = 640, 480
+screen = pygame.display.set_mode(screen_size)
+
+class Breakout(pygame.sprite.Sprite):
+  def __init__(self, color, width, height):
+    super().__init__()
+    
+    self.image = pygame.Surface([paddle_width, paddle_height)
+    self.image.fill(paddle_color)
+                                 
+  def __init__(self, color, width, height):
+    
+    super().__init__()
+    
+                                 
+
+
+brick_width = 64
+brick_height = 48
+paddle_width = 64
+paddle_height = 48
+ball_diameter = 20
 ball_radius = ball_diameter / 2
-# SETTING THE MAX DISTANCE THAT THE PADDLE AND THE BALL CAN GO ON THE SCREEN
-max_paddle_x = screen_size[0] - paddle_width
-max_ball_x = screen_size[0] - ball_diameter
-max_ball_y = screen_size[1] - ball_diameter
-#PADDLE Y COORDINATE... NOT BEING ABLE TO MOVE UP AND JUST RIGHT TO LEFT
-paddle_y = screen_size[1] - paddle_height - 10
-#DEFINING SOME COLORS
-black = (0, 0, 0)
-green = (0, 255, 0)
-purple = (255, 0, 255)
-brick_color = (0, 0, 255)
+ball_color = [255, 0, 255]
+ball_x = 50
+ball_y = 100
+
+paddle_color = [0, 255, 0]
+
+
 # GAME STATES
 state_ball_in_paddle = 0
 state_playing = 1
 state_won = 2
 state_over = 3
-
-class Breakout:
-  def __init__():
     
-    self.screen = pygame.display.set_mode(screen_size)
+   
     
     self.init_game()
     
   def init_game(self):
     self.lives = 3
     self.score = 0
-    self.state = state_ball_in_paddle
-    
-    self.paddle = pygame.Rect(200, paddle_y, paddle_height, paddle_width)
-    self.ball = pygame.Rect(200, paddle_y, ball_diameter, ball_diameter, ball_diameter)
-    
-    self.ball_vel = [5, -5]
-    self.create_bricks()
+
     
   def create_bricks(self):
     bricks_list = []
     for i in range(1,8):
       brick1 = Brick(75*i, 50)
       bricks_list.append(brick1)
-      
+      # DEFINING THE STRUCTURES 
+brick_width = 64
+brick_height = 48
+paddle_width = 64
+paddle_height = 48
+ball_diameter = 20
+ball_radius = ball_diameter / 2
+ball_color = [255, 0, 255]
+paddle_color = [0, 255, 0]
+
+# GAME STATES
+state_ball_in_paddle = 0
+state_playing = 1
+state_won = 2
+state_over = 3
+
+
    
       
       
