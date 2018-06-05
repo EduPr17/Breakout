2962ce33e4c226990166f2b9cc6d12d505aa9726
EduPr17 <31894505+EduPr17@users.noreply.github.com>
2018-06-05 12:27:09 -0400

    Update breakout.py

--- breakout.py
+++ breakout.py
@@ -1,8 +1,5 @@
 import pygame
 pygame.init()
-#SCREEN SIZE
-screen_size = 800, 600
-screen = pygame.display.set_mode(screen_size)
 
 class Breakout(pygame.sprite.Sprite):
   def __init__(self, color, x, y):
@@ -33,7 +30,7 @@ class Ball(pygame.sprite.Sprite):
       
     self.image = pygame.Surface([self.width, self.height])
     
-    self.image,fill(green)
+    self.image.fill(green)
     
     self.rect = self.image.get_rect()
     
@@ -43,7 +40,28 @@ class Ball(pygame.sprite.Sprite):
     def bouncing(self, diff):
       self.direction = (180 - self.direction) % 360
       self.direction -= diff
+    
+    self.rect.x = self.x
+    self.rect.y = self.y
+    
+    if self.y <= 0:
+      self.bounce(0)
+      self.y = 1
+    
+    if self.x <= 0:
+      self.direction = (360 - self.direction) % 360
+      self.x = 1
+    
+    if self.x > self.screenwidth - self.width:
+      self.rect.x = self.screenwidth - self.width
+    
+pygame.init()
 
+screen
+
+      
+      
+      
 brick_width = 64
 brick_height = 48
 paddle_width = 64
